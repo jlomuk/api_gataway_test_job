f6b98d8ad380bcd495111d6dbdd01a13b1dc7fba
Denis Dolgopolov <jlomuk1990@gmail.com>
2022-10-27 15:35:32 +0300

    minor edit settings

--- src/settings.py
+++ src/settings.py
@@ -5,10 +5,7 @@ class Settings(BaseSettings):
     USER_BACKEND_SERVICE: AnyHttpUrl = ''
     TASK_BACKEND_SERVICE: AnyHttpUrl = ''
 
-    SECRET_KEY: SecretStr = 'some_secret'
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 5
-
-    class Config():
+    class Config:
         env_file = '.env'
 
 
