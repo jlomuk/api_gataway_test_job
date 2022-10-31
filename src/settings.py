682c8f0c8a8d8c7ac00706703e083419e4ca50be
Denis Dolgopolov <jlomuk1990@gmail.com>
2022-10-31 15:33:34 +0300

    created ci, fix code

--- src/settings.py
+++ src/settings.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings, EmailStr, SecretStr, validator, AnyHttpUrl
 
 
 class Settings(BaseSettings):
-    USER_BACKEND_SERVICE: AnyHttpUrl = ''
-    TASK_BACKEND_SERVICE: AnyHttpUrl = ''
+    USER_BACKEND_SERVICE: str = ''
+    TASK_BACKEND_SERVICE: str = ''
 
     class Config:
         env_file = '.env'
